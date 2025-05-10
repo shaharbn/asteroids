dd919a4308fd2c3459db4bc75cb06d770a83ce63
shaharbn <shahar.barnatan@gmail.com>
2025-05-10 15:50:33 +0300

    final project

--- circleshape.py
+++ circleshape.py
@@ -20,3 +20,8 @@ class CircleShape(pygame.sprite.Sprite):
     def update(self, dt):
         # sub-classes must override
         pass
+
+    def collision(self, other_circle):
+        distance = self.position.distance_to(other_circle.position)
+        total_radiuses = self.radius + other_circle.radius
+        return distance <= total_radiuses
--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+import sys
+from shot import Shot
 
 def main():
     pygame.init()
@@ -10,19 +14,41 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable,)
+    Shot.containers = (shots, updatable, drawable)
+
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroid_field = AsteroidField()
+
     while True:
         # Check for player inputs
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         
-        player.update(dt)
-
+        updatable.update(dt)
 
         # Update the game world
         screen.fill("black")
-        player.draw(screen)
+        for d in drawable:
+            d.draw(screen)
+        for asteroid in asteroids:
+            if player.collision(asteroid):
+                print("Game over!")
+                sys.exit(1)
+            for shot in shots:
+                if asteroid.collision(shot):
+                    shot.kill()
+                    asteroid.split()
 
         # Draw the game to the screen
         pygame.display.flip()
